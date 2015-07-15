1fbed9b65366ba51ea7125950aa39a73c8d48ff5
Tim Mathews <tmathews@impaqint.com>
2015-07-15 18:38:39 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import App from './App';
 
-React.render(<App />, document.getElementById('root'));
+React.render(<App value={123.4567} units={'m/s'} label={'Velocity'} />, document.getElementById('root'));
